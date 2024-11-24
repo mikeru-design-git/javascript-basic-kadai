4b5d4c599503f114254cf0370f49cb688385702c
mikeru-design-git <design.mikeru@gmail.com>
2024-11-24 16:14:34 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,17 @@
+
+
+
+const squared = (num) => {
+  console.log(num **2);
+}
+
+squared(10);
+
+
+// 【別案】
+// const squared = (num) => {
+//   const result = num **2
+//   console.log(result);
+// }
+
+// squared(10);
